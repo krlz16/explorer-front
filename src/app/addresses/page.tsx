b65d7ecb4a9a8e442a24f99da3ea50b1a7920b4f
Sebastian Guaqueta <sebastian.guaqueta@rootstocklabs.com>
2025-01-28 15:48:50 -0500

    adding title component

--- src/app/addresses/page.tsx
+++ src/app/addresses/page.tsx
@@ -7,22 +7,17 @@ import { fetchAddresses } from '@/services/addresses';
 export default async function Page(props: IPageProps) {
   const params = await props.searchParams;
   console.log('params are: ', params);
-  
+
   const response = await fetchAddresses(params);
   console.log('response: ', response?.pagination);
   return (
-    <div className='w-full'>
+    <div className="w-full">
       <h1 className="font-bold text-3xl flex gap-3 items-center mt-10">
-        <AddressIcon className='w-6 h-6' />
+        <AddressIcon className="w-6 h-6" />
         Addresses
       </h1>
-      <Pagination
-        text='Total Addresses'
-        data={response!.pagination!}
-      />
-      <AddressesTable
-        addresses={response?.data}
-      />
+      <Pagination text="Total Addresses" data={response!.pagination!} />
+      <AddressesTable addresses={response?.data} />
     </div>
   );
 }
--- src/app/verify/[address]/layout.tsx
+++ src/app/verify/[address]/layout.tsx
@@ -1,25 +1,35 @@
-import { ROUTER } from "@/common/constants"
-import { AddressIcon } from "@/common/icons"
-import { ADDRESSES_BTN_TABS } from "@/components/addresses/tabs/AddressesTabs"
-import Card from "@/components/ui/Card"
-import PageHeader from "@/components/page/PageHeader"
-import { fetchAddress } from "@/services/addresses"
+import { ROUTER } from '@/common/constants';
+import { AddressIcon } from '@/common/icons';
+import { ADDRESSES_BTN_TABS } from '@/components/addresses/tabs/AddressesTabs';
+import Card from '@/components/ui/Card';
+import PageHeader from '@/components/page/PageHeader';
+import { fetchAddress } from '@/services/addresses';
+import ToolTip from '@/components/ui/ToolTip';
 
 type props = {
   params: Promise<{
-    address: string
-  }>,
-  children: React.ReactNode
-}
+    address: string;
+  }>;
+  children: React.ReactNode;
+};
 
-export default async function layout({ children, params }:props) {
+export default async function layout({ children, params }: props) {
   const addressParam = (await params).address;
   const response = await fetchAddress(addressParam);
   const address = response?.data;
 
   return (
     <Card pd="p0">
-      { children }
+      <h1 className="flex gap-3 items-center text-4xl mt-[40px]">
+        {'Verify Contract'.toUpperCase()}
+      </h1>
+      <div className="text-white-400 mt-6">
+        {'Contract addess'}
+        <span className="text-brand-green ml-1">
+          <ToolTip text={address?.address} trim={0} />
+        </span>
+      </div>
+      {children}
     </Card>
-  )
+  );
 }
