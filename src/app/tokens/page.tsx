565a9ad74cff87cd9808a080e6c17820831fd85d
ivegabr <126865465+ivegabr@users.noreply.github.com>
2025-02-17 11:29:01 +0100

    add token pagination (#15)

--- src/app/tokens/page.tsx
+++ src/app/tokens/page.tsx
@@ -2,13 +2,24 @@ import Pagination from '@/components/ui/Pagination';
 import { IPageProps } from '@/common/interfaces/RouterParams';
 import TokensTable from '@/components/tokens/TokensTable';
 import { fetchTokens } from '@/services/tokens';
+import PaginationCursor from '@/components/ui/PaginationCursor';
 
 export default async function page(props: IPageProps) {
   const params = await props.searchParams;
   const response = await fetchTokens(params);
   return (
     <div className="w-full">
-      <Pagination text="Total tokens" data={response!.pagination!} />
+      <div className="mt-8">
+        <PaginationCursor
+          data={
+            response?.paginationData || {
+              nextCursor: response?.paginationData?.nextCursor ?? null,
+              prevCursor: response?.paginationData?.prevCursor ?? null,
+              take: response?.paginationData?.take ?? 50,
+            }
+          }
+        />
+      </div>
       <TokensTable tokens={response?.data} />
     </div>
   );
