193e2c78694c447e9975703ea64fe7d39db9f1bb
ivegabr <isidro.vega@iovlabs.org>
2025-02-17 11:59:45 +0100

    small changes

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,30 +1,62 @@
-import { DiscordIcon, GithubIcon, XIcon } from '@/common/icons'
-import Link from 'next/link'
-import React from 'react'
+import { DiscordIcon, GithubIcon, XIcon } from '@/common/icons';
+import Link from 'next/link';
+import React from 'react';
 
 function Footer() {
   return (
-    <footer className='w-full md:flex justify-between items-center p-6 bg-primary relative z-40 border-t border-t-line text-sm'>
+    <footer className="w-full md:flex justify-between items-center p-6 bg-primary relative z-40 border-t border-t-line text-sm">
       <div>
-        <div className='flex gap-2 items-center'>
+        <div className="flex gap-2 items-center">
           <span>Built by</span>
-          <span className='text-lg font-bold'>RootstockLabs</span>
+          <span className="text-lg font-bold">RootstockLabs</span>
         </div>
-        <div>Copyright &copy; 2024 Roostock Labs. All rights reserved.</div>
+        <div className="text-xs text-gray-400">
+          Copyright &copy; 2025 RoostockLabs. All rights reserved.
+        </div>
       </div>
-      <div className='flex gap-2 flex-wrap mt-3'>
-        <Link className='hover:underline' href='https://rootstock.io/'>About Rootstock Labs</Link>
-        <Link className='hover:underline' href='https://rootstock.io/contact/'>Help</Link>
-        <Link className='hover:underline' href='https://rootstock.io/terms-conditions/'>Terms & Conditions</Link>
-        <Link className='hover:underline' href='https://dev.rootstock.io/'>Documentation</Link>
+      <div className="flex gap-4 flex-wrap mt-3">
+        <Link
+          className="hover:underline"
+          href="https://rootstock.io/"
+          target="_blank"
+        >
+          About RootstockLabs
+        </Link>
+        <Link
+          className="hover:underline"
+          href="https://rootstock.io/contact/"
+          target="_blank"
+        >
+          Help
+        </Link>
+        <Link
+          className="hover:underline"
+          href="https://rootstock.io/terms-conditions/"
+          target="_blank"
+        >
+          Terms & Conditions
+        </Link>
+        <Link
+          className="hover:underline"
+          href="https://dev.rootstock.io/"
+          target="_blank"
+        >
+          Documentation
+        </Link>
       </div>
-      <div className='flex gap-4 mt-6'>
-        <Link href='https://twitter.com/rootstock_io'><XIcon /></Link>
-        <Link href='https://github.com/rsksmart'><GithubIcon /></Link>
-        <Link href='https://discord.com/invite/rootstock'><DiscordIcon /></Link>
+      <div className="flex gap-4 mt-6">
+        <Link href="https://twitter.com/rootstock_io" target="_blank">
+          <XIcon />
+        </Link>
+        <Link href="https://github.com/rsksmart" target="_blank">
+          <GithubIcon />
+        </Link>
+        <Link href="https://discord.com/invite/rootstock" target="_blank">
+          <DiscordIcon />
+        </Link>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
