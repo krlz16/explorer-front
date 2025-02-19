929f9465b3f21f6081e801c50fdf2c71c0be8222
ivegabr <126865465+ivegabr@users.noreply.github.com>
2025-02-19 16:06:34 +0100

    Feat/DV-799: addresses>tokens and addresses>accounts tabs (#18)
    * add addresses>tokens tab and table
    
    * add addresses>accounts tab
    
    * fix lint errors

--- src/components/addresses/tabs/AddressesTxsTabsContent.tsx
+++ src/components/addresses/tabs/AddressesTxsTabsContent.tsx
@@ -7,6 +7,9 @@ import InternalTxsTable from '@/components/itxs/InternalTxsTable';
 import TokenTransfersTable from '@/components/transfer/TokenTransfersTable';
 import TxsTable from '@/components/txs/TxsTable';
 import React from 'react';
+import TokensByAddressTable from './TokensByAddressTable';
+import { ITokensByAddress } from '@/common/interfaces/Tokens';
+import AccountsByAddressTable from './AccountsTable';
 
 type props = {
   currentTab: string;
@@ -15,19 +18,27 @@ type props = {
   events: IEvents[] | undefined;
   tokens: IEvents[] | undefined;
   balances: IBalances[] | undefined;
+  tokensByAddress: ITokensByAddress[] | undefined;
+  accountsByAddress: ITokensByAddress[] | undefined;
 };
 
 const AddressesTxsTabsContent = ({
   currentTab,
   txs,
   itxs,
+  tokensByAddress,
+  accountsByAddress,
   events,
   tokens,
   balances,
 }: props) => {
   if (currentTab === 'txs') return <TxsTable txs={txs} />;
   if (currentTab === 'itxs') return <InternalTxsTable itxs={itxs} />;
+  if (currentTab === 'tokens')
+    return <TokensByAddressTable tokensByAddress={tokensByAddress} />;
   if (currentTab === 'events') return <EventsTable events={events} />;
+  if (currentTab === 'accounts')
+    return <AccountsByAddressTable accountsByAddress={accountsByAddress} />;
   if (currentTab === 'token_transfer')
     return <TokenTransfersTable tokens={tokens} />;
   if (currentTab === 'balances') return <BalancesTable balances={balances} />;
